0d465c796d83c1a96cea8fb317242d7211e1a449
celeri <celestin.bouchet@gmail.com>
2026-06-03 09:10:26 +0200

    refactor search-tmdb function

--- search-tmdb/handler.js
+++ search-tmdb/handler.js
@@ -1,22 +1,50 @@
 'use strict'
 
-module.exports = async (event, context) => {
-  const fs = require('node:fs');
+const fs = require('node:fs/promises');
 
-  // Get TMDB access token from openfaas secret
-  let tmdbAccessToken;
-  try {
-    tmdbAccessToken = fs.readFileSync('/var/openfaas/secrets/tmdb-access-token', 'utf8');
-  } catch (err) {
-    return context.status(500);
+module.exports = async (event, context) => {
+  const tmdbAccessToken = await getTmdbAccessToken();
+
+  if (!tmdbAccessToken) {
+    return context.fail("Missing TMDB access token");
   }
 
+  const result = await searchMovies(tmdbAccessToken, {
+    query: event.body.query ?? '',
+  });
+
+  if (!result && result.success === false) {
+    return context.fail(result?.status_message ?? 'An error occured while querying TMDB');
+  }
+
+  return context
+    .status(200)
+    .succeed(JSON.stringify(result.results));
+}
+
+/**
+ * Get TMDB access token from openfaas secret
+ */
+async function getTmdbAccessToken() {
+  try {
+    return await fs.readFile('/var/openfaas/secrets/tmdb-access-token', { encoding: 'utf8' });
+  } catch (err) {
+    return undefined;
+  }
+}
+
+/**
+ * Search movies on TMDB
+ * @param {string} tmdbAccessToken 
+ * @param {{ query: string }} params 
+ */
+async function searchMovies(tmdbAccessToken, params) {
   // Build url with query
   const url = new URL('https://api.themoviedb.org/3/search/movie');
   url.searchParams.set('include_adult', 'true');
   url.searchParams.set('language', 'fr-FR');
   url.searchParams.set('page', 1);
-  url.searchParams.set('query', event.body.query ?? '');
+  url.searchParams.set('query', params.query);
 
   // Build headers with access token
   const headers = {
@@ -24,16 +52,15 @@ module.exports = async (event, context) => {
     'Authorization': `Bearer ${tmdbAccessToken}`,
   };
 
-  // Call TMDB
-  const res = await fetch(url, {
-    method: 'GET',
-    headers,
-  })
-  .then(res => res.json());
-
-  const result = JSON.stringify(res.results?.map(m => m.title));
-
-  return context
-    .status(200)
-    .succeed(result);
+  try {
+    // Call TMDB
+    return await fetch(url, {
+      method: 'GET',
+      headers,
+    })
+    .then(res => res.json());
+  } catch (err) {
+    console.error("TMDB request failed:", err);
+    return undefined;
+  }
 }
